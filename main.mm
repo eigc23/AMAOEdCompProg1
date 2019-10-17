00abb10750880069a162dabfac370f78eb07b664
eigc23 <56584313+eigc23@users.noreply.github.com>
2019-10-17 23:56:17 +0800

    Add files via upload
    Division problem

--- main.mm
+++ main.mm
@@ -1,39 +1,41 @@
 //
 //  main.mm
-//  switch
+//  divide
 //
 //  Created by Eira Isabel Cuartero on 10/17/19.
 //  Copyright © 2019 Isabelle. All rights reserved.
 //
 
 #include <iostream>
+// include iostream problem with my OS and laptop which I cannot fix but compiling and running still works
 
 using namespace std;
 
-int main() {
-    int option;
-    std::cout << "Please enter an integer\n";
-    std::cin >> option;
+int main(int argc, const char * argv[])
+{
+    //division
+    float dividend, divisor, max, min;
+    max = std::numeric_limits<float>::max();
+    min = std::numeric_limits<float>::min();
     
-    switch (option)
-    {
-        case 0:
-            std::cout << "Hello World" << endl;
-            break;
-        case 1:
-            std::cout << "I am good" << endl;
-            break;
-        case 2:
-            std::cout << "To the Top" << endl;
-            break;
-        case 3:
-            std::cout << "Where is the horizon" << endl;
-            break;
-        case 4:
-            std::cout << "I do not know" << endl;
-            break;
-        default:
-            std::cout << "Yeah, I will.";
-            break;
-    }
-}
+    std::cout << "Please enter your dividend: \n";
+    std::cin >> dividend;
+    
+    std::cout << "Please enter your divisor: \n";
+    std::cin >> divisor;
+    
+    
+    if (divisor == 0){
+        std::cout << "Divisor should not be zero." << endl;
+    } else if (dividend > max)
+        std::cout << "Dividend value should not be greater than maximum allowed value. Try again" << endl;
+
+    else if (dividend < min)
+        std::cout << "Dividend value should not be less than minimum allowed value. Try again" << endl;
+    else if (divisor > min)
+    std::cout << "Divisor value should not be greater than minimum allowed value. Try again." << endl;
+    else if (divisor > max)
+    std::cout << "Divisor value should not be greater than maximum allowed value. Try again";
+    
+std::cout << "Your quotient is: " << dividend / divisor << endl;
+     }
